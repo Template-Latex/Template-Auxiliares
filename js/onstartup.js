b3a0e8ccfda723e3445fa3a85bc7b35bf7d4ab51
Pablo Pizarro <pablo.pizarro@ing.uchile.cl>
2017-07-22 03:52:43 +0100

    Actualizacion a latex subdomain

--- js/onstartup.js
+++ js/onstartup.js
@@ -1,9 +1,9 @@
 // Configuraciones
-var href_images_parent = 'http://ppizarror.com/Template-Informe/';
-var href_json_releases = 'https://api.github.com/repos/ppizarror/Template-Auxiliares/releases';
-var href_pdf_version = 'https://github.com/ppizarror/Template-Informe/raw/master/pdf_version/Auxiliares/Template v{0}.pdf';
-var href_github_project = 'https://github.com/ppizarror/Template-Auxiliares/releases/';
-var href_github_project_source = 'https://github.com/ppizarror/Template-Auxiliares/';
+var href_images_parent = 'http://latex.ppizarror.com/Template-Informe/';
+var href_json_releases = 'https://api.github.com/repos/Template-Latex/Template-Auxiliares/releases';
+var href_pdf_version = 'https://github.com/ppizarror/Template-Latex/raw/master/pdf_version/Auxiliares/Template v{0}.pdf';
+var href_github_project = 'https://github.com/Template-Latex/Template-Auxiliares/releases/';
+var href_github_project_source = 'https://github.com/Template-Latex/Template-Auxiliares/';
 
 // Se selecciona una imagen al azar
 var images_background = [
