9e19d18ed9cfe3b22df7d7b41a23e38c835ce5ef
Pablo Pizarro <pablo.pizarro@ing.uchile.cl>
2017-07-27 22:47:48 +0100

    Se cambia ubicacion pdf

--- js/onstartup.js
+++ js/onstartup.js
@@ -1,7 +1,7 @@
 ﻿// Configuraciones
 var href_images_parent = 'http://latex.ppizarror.com/Template-Informe/';
 var href_json_releases = 'https://api.github.com/repos/Template-Latex/Template-Auxiliares/releases';
-var href_pdf_version = 'https://github.com/ppizarror/Template-Latex/raw/master/pdf_version/Auxiliares/Template v{0}.pdf';
+var href_pdf_version = 'https://github.com/Template-Latex/pdf-version/raw/master/Auxiliares/Template-Auxiliares v{0}.pdf';
 var href_github_project = 'https://github.com/Template-Latex/Template-Auxiliares/releases/';
 var href_github_project_source = 'https://github.com/Template-Latex/Template-Auxiliares/';
 
